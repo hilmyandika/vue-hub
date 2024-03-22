cd3bad7ae5d15808ed362e3df94944a8b7d4a333
hilmyandika <hilmyandika25@gmail.com>
2024-03-22 10:44:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>
+    Hello Kelas F
+  </h1>
+  <TextH/>
+
+  <br>
+
+  <ButtonC/>
+</template>
+
+<script setup>
+import ButtonC from "./components/Buttonc.vue"
+import TextH from "./components/TextHeader.vue"
+
+</script>
---
+++ src/components/Buttonc.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+
+<style scoped>
+.btn {
+    background-color: red;
+    color: white;
+    font-size: 24px;
+}
+</style>
